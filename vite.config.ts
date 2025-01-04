a25d68295bf6ded5b6e8bc841d6193c1bcc7f6e7
Chimder <dimas.kirilyuk@gmail.com>
2025-01-04 15:53:28 +0000

    cache

--- vite.config.ts
+++ vite.config.ts
@@ -17,67 +17,43 @@ export default defineConfig({
     },
   },
 
+  // cacheDir: path.resolve(__dirname, 'node_modules/.vite'),
+
   server: {
     port: 3000,
   },
 
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      // output: {
-      //   manualChunks(id) {
-      //     if (
-      //       id.includes('node_modules/react') ||
-      //       id.includes('node_modules/react-dom') ||
-      //       id.includes('node_modules/react-router')
-      //     ) {
-      //       return 'core'
-      //     }
-      //     if (id.includes('node_modules')) {
-      //       if (
-      //         id.includes('@radix-ui') ||
-      //         id.includes('lucide-react') ||
-      //         id.includes('class-variance-authority')
-      //       ) {
-      //         return 'ui'
-      //       }
-      //       if (id.includes('@tanstack/react-query') || id.includes('axios')) {
-      //         return 'data'
-      //       }
-      //       return 'vendor'
-      //     }
-      //     if (id.includes('/components/Layout')) {
-      //       return 'layout'
-      //     }
-      //     if (id.includes('/components/')) {
-      //       return 'components'
-      //     }
-      //     return null
-      //   },
-      //   chunkFileNames: chunkInfo => {
-      //     if (
-      //       chunkInfo.name === 'core' ||
-      //       chunkInfo.name === 'ui' ||
-      //       chunkInfo.name === 'data' ||
-      //       chunkInfo.name === 'vendor' ||
-      //       chunkInfo.name === 'layout' ||
-      //       chunkInfo.name === 'components'
-      //     ) {
-      //       return 'assets/[name]-[hash].js'
-      //     }
-      //     return 'assets/[name].js'
-      //   },
-      // },
-    },
-    modulePreload: {
-      polyfill: false,
-    },
-  },
+  // build: {
+  //   outDir: 'dist',
+  // rollupOptions: {
+  // cache: true,
+  // },
+  // modulePreload: {
+  //   polyfill: false,
+  // },
+  // },
 
   preview: {
     port: 3000,
     strictPort: true,
     open: true,
     cors: true,
+  },
+
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      '@tanstack/react-query',
+      '@tanstack/react-query-devtools',
+      'axios',
+      'zustand',
+      'immer',
+      'clsx',
+      'classnames',
+      'dayjs',
+    ],
+    exclude: ['@radix-ui/themes'],
   },
 })
