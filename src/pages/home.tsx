58c35ab582692a5a8ba5cc5731c607d9a94c5fb3
Chimder <dimas.kirilyuk@gmail.com>
2025-01-15 21:00:06 +0000

    del swiper

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,14 @@
-import { EmblaOptionsType } from 'embla-carousel'
-
 import { mangaApi } from '@/hooks/api/mangadex/manga'
-import Carousel from '@/components/swiper/Carousel'
-import EmblaCarousel from '@/components/swiper/EmblaCarousel'
 
 export const Home = () => {
   const { data } = mangaApi.useMangaSearchPopular()
-  const OPTIONS: EmblaOptionsType = { align: 'start' }
 
   return (
     <div>
       <h1>Home</h1>
+      {/* {data?.data && <Carousell mangas={data.data} />} */}
       {/* <EmblaCarousel slides={data?.data} options={OPTIONS} /> */}
-      {data?.data && <Carousel data={data.data} key={`fsdfsfsdfds`} />}
+      {/* {data?.data && <Carousel data={data.data} key={`fsdfsfsdfds`} />} */}
     </div>
   )
 }
